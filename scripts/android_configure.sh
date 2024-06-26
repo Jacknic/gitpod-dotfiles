380b818341b051b92570a3b70abee0b4b9a3e850
Jacknic <jacknic@qq.com>
2024-06-26 17:08:52 +0000

    调整环境变量

--- scripts/android_configure.sh
+++ scripts/android_configure.sh
@@ -3,8 +3,8 @@
 # default JDK is 11
 # https://github.com/gitpod-io/workspace-images/blob/a1a0eee4d93c8410cabc2b1d2fb882080e07e62d/dazzle.yaml#L32
 # export JAVA_HOME=${HOME}/.sdkman/candidates/java/current/
-ANDROID_HOME=${HOME}/.android-sdk
-ANDROID_SDK_ROOT=$ANDROID_HOME
+export ANDROID_HOME=${HOME}/.android-sdk
+export ANDROID_SDK_ROOT=$ANDROID_HOME
 CMD_TOOLS_DIR=${ANDROID_HOME}/cmdline-tools
 CMD_TOOLS_FILE=android-commandline-tools.zip
 mkdir -p ${CMD_TOOLS_DIR}
@@ -13,7 +13,7 @@ cd /tmp/ &&
     unzip -q $CMD_TOOLS_FILE -d . &&
     mv cmdline-tools/ ${CMD_TOOLS_DIR}/latest &&
     rm *.zip
-
+export PATH=${PATH}:${CMD_TOOLS_DIR}/latest/bin
 yes | sdkmanager --licenses
 yes | sdkmanager --update
 
